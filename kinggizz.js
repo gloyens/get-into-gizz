d525d68bae0eaa9087d31b8a62392c132c4ba9e5
George <92999775+gloyens@users.noreply.github.com>
2022-07-25 18:39:36 +0100

    Add images; random album functionality

--- kinggizz.js
+++ kinggizz.js
@@ -1,7 +1,20 @@
 function openMenu() {
-    document.getElementById("menu").style.height = "100%";
-  }
+  document.getElementById("menu").style.height = "100%";
+}
   
-  function closeMenu() {
-    document.getElementById("menu").style.height = "0%";
+function closeMenu() {
+  document.getElementById("menu").style.height = "0%";
+}
+
+function randomAlbum() {
+  var pages = ["mind-fuzz.html", 
+    "nonagon-infinity.html",
+    "paper-mache-dream-balloon.html"];
+
+  var value = pages[Math.floor(Math.random() * pages.length)];
+
+  if (window.location == "index.html") {
+    window.location = "album-pages/" + value;
   }
+  else {window.location = "../album-pages/" + value;}
+}
