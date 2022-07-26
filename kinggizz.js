20b4a566717b75f331293779d4071c5b092e7e0f
George <92999775+gloyens@users.noreply.github.com>
2022-07-26 17:16:44 +0100

    Add album pages 4-7

--- kinggizz.js
+++ kinggizz.js
@@ -7,9 +7,13 @@ function closeMenu() {
 }
 
 function randomAlbum() {
-  var pages = ["album-pages/mind-fuzz.html", 
+  var pages = ["album-pages/butterfly-3000.html",
+    "album-pages/im-in-your-mind-fuzz.html",
+    "album-pages/infest-the-rats-nest.html", 
     "album-pages/nonagon-infinity.html",
-    "album-pages/paper-mache-dream-balloon.html"];
+    "album-pages/paper-mache-dream-balloon.html",
+    "album-pages/polygondwanaland.html",
+    "album-pages/quarters.html"];
 
   var value = pages[Math.floor(Math.random() * pages.length)];
   console.log(window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')));
