ba3299f17e370b1c241ae7977d095ecff0492930
George <92999775+gloyens@users.noreply.github.com>
2022-07-28 18:07:19 +0100

    Update random page function to include all new pages

--- kinggizz.js
+++ kinggizz.js
@@ -7,13 +7,27 @@ function closeMenu() {
 }
 
 function randomAlbum() {
-  var pages = ["album-pages/butterfly-3000.html",
+  var pages = ["album-pages/12-bar-bruise.html",
+    "album-pages/butterfly-3000.html",
+    "album-pages/eyes-like-the-sky.html",
+    "album-pages/fishing-for-fishies.html",
+    "album-pages/float-along-fill-your-lungs.html",
+    "album-pages/flying-microtonal-banana.html",
+    "album-pages/gumboot-soup.html",
     "album-pages/im-in-your-mind-fuzz.html",
-    "album-pages/infest-the-rats-nest.html", 
+    "album-pages/infest-the-rats-nest.html",
+    "album-pages/kg.html",
+    "album-pages/lw.html",
+    "album-pages/made-in-timeland.html",
+    "album-pages/murder-of-the-universe.html",
     "album-pages/nonagon-infinity.html",
+    "album-pages/oddments.html",
+    "album-pages/omnium-gatherum.html",
     "album-pages/paper-mache-dream-balloon.html",
     "album-pages/polygondwanaland.html",
-    "album-pages/quarters.html"];
+    "album-pages/quarters.html",
+    "album-pages/sketches-of-brunswick-east.html",
+    "album-pages/willoughbys-beach.html"];
 
   var value = pages[Math.floor(Math.random() * pages.length)];
   console.log(window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')));
